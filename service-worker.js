5845e107a7e86a408acf535853d97e313213b4d9
Aurelia <279377517+aureliaspirit@users.noreply.github.com>
2026-05-25 14:58:27 -0400

    Upgrade moonlight wardrobe panel

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-﻿const CACHE_NAME = "heartbox-pwa-v1-9-22";
+﻿const CACHE_NAME = "heartbox-pwa-v1-9-23";
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=1.9.22",
-  "./app.js?v=1.9.22",
-  "./manifest.json?v=1.9.22",
+  "./style.css?v=1.9.23",
+  "./app.js?v=1.9.23",
+  "./manifest.json?v=1.9.23",
   "./icons/icon-120.png",
   "./icons/icon-152.png",
   "./icons/icon-167.png",
