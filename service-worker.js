cfd3163eea12fac4ea93a2aa14ec83a4558e67fd
Aurelia <279377517+aureliaspirit@users.noreply.github.com>
2026-05-25 21:46:14 -0400

    Add lucky pouch wardrobe notes

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-﻿const CACHE_NAME = "heartbox-pwa-v1-9-25";
+﻿const CACHE_NAME = "heartbox-pwa-v1-9-26";
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=1.9.25",
-  "./app.js?v=1.9.25",
-  "./manifest.json?v=1.9.25",
+  "./style.css?v=1.9.26",
+  "./app.js?v=1.9.26",
+  "./manifest.json?v=1.9.26",
   "./icons/icon-120.png",
   "./icons/icon-152.png",
   "./icons/icon-167.png",
